a8d6bbe4ddb609323c9c6cd6d4825ac375f57c6f
Max Golden <maxgolden@gmail.com>
2014-06-21 18:06:36 -0700

    major cleanup and optimisation pass

--- imageart.go
+++ imageart.go
@@ -7,11 +7,44 @@ import (
 	"image/png"
 	"math"
 	"os"
-	"time"
 )
 
+const (
+	// caps: red < MaxRed
+	MaxRed   = 256
+	MaxGreen = 256
+	MaxBlue  = 256
+
+	// filled alpha value: a = FullAlpha
+	FullAlpha = 255
+
+	MaxHeight = 4096
+	MaxWidth  = 4096
+
+	// r-g-b value of first seed point
+	FirstRed   = 0
+	FirstGreen = 0
+	FirstBlue  = 0
+
+	StartX = 0
+	StartY = 0
+
+	ChanSize = 8
+
+	PicName = "art.png"
+)
+
+func sqr(x int32) int32 {
+	return x * x
+}
+
 func distance(a, b, c, x, y, z int32) (d float64) {
-	d = math.Sqrt(math.Pow(float64(a-x), 2) + math.Pow(float64(b-y), 2) + math.Pow(float64(c-z), 2))
+	d = math.Sqrt(float64(sqr(a-x) + sqr(b-y) + sqr(c-z)))
+	return
+}
+
+func mhtnDistance(a, b, c, x, y, z int32) (d float64) {
+	d = math.Abs(float64(a-x)) + math.Abs(float64(b-y)) + math.Abs(float64(c-z))
 	return
 }
 
@@ -24,7 +57,7 @@ func maxint(a, b int32) int32 {
 
 func draw(pic *image.NRGBA) {
 	fmt.Println("Drawing...")
-	file, err := os.Create("newpic.png")
+	file, err := os.Create(PicName)
 
 	if err != nil {
 		panic(err)
@@ -39,71 +72,47 @@ func draw(pic *image.NRGBA) {
 	}
 }
 
-func logTimestamp(start, end time.Time, count int32) {
+func fillPixelArray(pArray *PixelArray, cube *RGBCube, ch chan image.Point) (count int32) {
+	for count = 0; count < MaxWidth*MaxHeight; count++ {
+		point := <-ch
 
-	duration := end.Sub(start)
-
-	file, err := os.OpenFile("log.txt", 0, os.ModeAppend)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = file.WriteString(fmt.Sprintf("filled %d pixels in %s\n", count, duration))
-	if err != nil {
-		panic(err)
-	}
-
-	err = file.Close()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func popAndDraw(pic *image.NRGBA, colours *RGBCube, c chan image.Point, queuedList *QueuedArray) (count int32) {
-	for count = 0; count < 256*256*256; count++ {
-		point := <-c
-		// check that it hasn't already been filled
-		_, _, _, a := pic.At(point.X, point.Y).RGBA()
-		if a > 0 {
+		if pArray.FilledAt(int32(point.X), int32(point.Y)) {
 			count--
 			continue
 		}
-		// got one
-		// guaranteed to have SOME neighbours unless it's the first point
+
 		var red, green, blue int32
 		if count == 0 {
-			red = 0
-			green = 0
-			blue = 0
+			red = FirstRed
+			green = FirstGreen
+			blue = FirstBlue
 		} else {
-			red, green, blue = getTargetColour(point.X, point.Y, pic)
+			red, green, blue = pArray.TargetColourAt(int32(point.X), int32(point.Y))
 		}
-		fmt.Printf("target: %d %d %d\n", red, green, blue)
-		red, green, blue = nearestAvailableColour(red, green, blue, colours)
-		fmt.Printf("using: %d %d %d\n", red, green, blue)
 
-		pic.Set(point.X, point.Y, color.NRGBA{uint8(red), uint8(green), uint8(blue), 255})
+		red, green, blue = nearestAvailableColour(red, green, blue, cube)
 
-		go repopulateChannel(c, point, pic, queuedList)
+		if count%(MaxWidth*MaxHeight/8) == 0 {
+			fmt.Printf("count: %d | Painting %d, %d, %d\n", count, red, green, blue)
+		}
+		pArray.Set(int32(point.X), int32(point.Y), red, green, blue)
+
+		go repopulateChannel(ch, point, pArray)
 	}
-	draw(pic)
 	return
 }
 
-func repopulateChannel(c chan image.Point, point image.Point, pic *image.NRGBA, queuedList *QueuedArray) {
+func repopulateChannel(ch chan image.Point, point image.Point, pArray *PixelArray) {
 	// repopulate channel wish FREE, NONQUEUED surrounding points
 
-	for i := -1; i < 2; i++ {
-		if point.X+i < 4096 && point.X+i >= 0 {
-			for j := -1; j < 2; j++ {
-				if point.Y+j < 4096 && point.Y+j >= 0 && !(i == 0 && j == 0) {
-					pt := image.Pt(point.X+i, point.Y+j)
-					current := pic.At(pt.X, pt.Y)
-					_, _, _, a := current.RGBA()
-					if a == 0 && !queuedList[pt.X][pt.Y] {
-						fmt.Printf("Adding %d, %d\n", pt.X, pt.Y)
-						queuedList[pt.X][pt.Y] = true
-						c <- pt
+	for x_offset := -1; x_offset < 2; x_offset++ {
+		if point.X+x_offset < MaxWidth && point.X+x_offset >= 0 {
+			for y_offset := -1; y_offset < 2; y_offset++ {
+				if point.Y+y_offset < MaxHeight && point.Y+y_offset >= 0 && !(x_offset == 0 && y_offset == 0) {
+					pt := image.Pt(point.X+x_offset, point.Y+y_offset)
+					if !pArray.FilledAt(int32(pt.X), int32(pt.Y)) && !pArray.QueuedAt(int32(pt.X), int32(pt.Y)) {
+						pArray[pt.X][pt.Y].Queued = true
+						ch <- pt
 					}
 				}
 			}
@@ -121,20 +130,20 @@ func nearestAvailableColour(r, g, b int32, colours *RGBCube) (red, green, blue i
 	} else {
 		// true = colour already used, need to find closest unused colour
 		// search colour 3space in increasing spherical shells
-		outer_radius := int32(0)  // radius of the search sphere
-		min_dist := float64(7095) // initialised farther than largest possible radius (sqrt(3) * 4096)
+		outer_radius := int32(0)             // radius of the search sphere
+		min_dist := float64(math.MaxFloat64) // initialised farther than largest possible radius
 		found := false
-		for !found && outer_radius < 256 {
+		for !found && outer_radius < MaxRed {
 			outer_radius++
 			// expand in sphere about src pt until a colour is false (that is to say, unused)
-			for i := maxint(0, r-outer_radius); i < r+outer_radius && i < 256; i++ {
+			for i := maxint(0, r-outer_radius); i < r+outer_radius && i < MaxRed; i++ {
 				// i traverses the end to end height of the sphere one level at a time i ~ r
-				inner_radius := math.Sqrt(math.Pow(float64(outer_radius), 2) + math.Pow(float64(i-r), 2))
+				inner_radius := math.Sqrt(float64(sqr(outer_radius) + sqr(i-r)))
 				// inner_radius = radius of (green-blue) circle at level i along sphere
-				for j := maxint(0, g-int32(inner_radius)); j < g+int32(inner_radius) && j < 256; j++ {
+				for j := maxint(0, g-int32(inner_radius)); j < g+int32(inner_radius) && j < MaxGreen; j++ {
 					// j traverses the end to end width of the gb circle along the g axis one row at a time j ~ g
-					secant_level := math.Sqrt(math.Pow(inner_radius, 2) + math.Pow(float64(j-g), 2))
-					for k := maxint(0, b-int32(secant_level)); k < b+int32(secant_level) && k < 256; k++ { // k ~ b
+					segment_level := math.Sqrt(float64(sqr(int32(inner_radius)) + sqr(j-g)))
+					for k := maxint(0, b-int32(segment_level)); k < b+int32(segment_level) && k < MaxBlue; k++ { // k ~ b
 						this_dist := distance(r, g, b, i, j, k)
 						// check that this point is not an interior point (one that was checked last time)
 						// then check that it hasn't been used
@@ -149,10 +158,10 @@ func nearestAvailableColour(r, g, b int32, colours *RGBCube) (red, green, blue i
 							}
 						} else {
 							// this_dist < outer radius - 1: this point was picked up in the previous shell
-							// loop on the similar points across the axis relative to the point
+							// loop on the similar points across the axis relative to the point, then break
 							// this is p hacky
-							num_checked := k - maxint(0, b-int32(secant_level))
-							for k = b + int32(secant_level) - num_checked; k < b+int32(secant_level) && k < 256; k++ {
+							num_checked := k - maxint(0, b-int32(segment_level))
+							for k = b + int32(segment_level) - num_checked; k < b+int32(segment_level) && k < MaxBlue; k++ {
 								if !colours[i][j][k] && this_dist < min_dist {
 									min_dist = this_dist
 									red = i
@@ -174,74 +183,109 @@ func nearestAvailableColour(r, g, b int32, colours *RGBCube) (red, green, blue i
 	return
 }
 
-func getTargetColour(x, y int, pic *image.NRGBA) (red, green, blue int32) {
+// cube representing png values in 3space
+// becomes true when a colour is placed in the image
+type RGBCube [MaxRed][MaxGreen][MaxBlue]bool
 
-	n := 0
-	pt_array := make([]image.Point, 8)
-	// fill pt_array with valid neighbours
-	for i := -1; i < 2; i++ {
-		if x+i < 4096 && x+i >= 0 {
-			for j := -1; j < 2; j++ {
-				if y+j < 4096 && y+j >= 0 && !(i == 0 && j == 0) {
-					pt_array[n] = image.Pt(x+i, y+j)
-					n++
+type Pixel struct {
+	Colour struct {
+		red   int32
+		green int32
+		blue  int32
+	}
+	Filled bool
+	Queued bool
+}
+
+func (c *Pixel) NRGBA() *color.NRGBA {
+	return &color.NRGBA{uint8(c.Colour.red), uint8(c.Colour.green), uint8(c.Colour.blue), FullAlpha}
+}
+
+// PixelArray is an array of the Pixel objects, and should be used to
+// interface with the underlying Pixel structure
+type PixelArray [MaxWidth][MaxHeight]Pixel
+
+func (c *PixelArray) ImageNRGBA() *image.NRGBA {
+	pic := image.NewNRGBA(image.Rect(0, 0, MaxWidth, MaxHeight))
+	for x := 0; x < MaxWidth; x++ {
+		for y := 0; y < MaxHeight; y++ {
+			pic.Set(x, y, c[x][y].NRGBA())
+		}
+	}
+	return pic
+}
+
+func (c *PixelArray) Set(x, y, red, green, blue int32) {
+	c[x][y].Colour.red = red
+	c[x][y].Colour.green = green
+	c[x][y].Colour.blue = blue
+	c[x][y].Filled = true
+}
+
+func (c *PixelArray) ColourAt(x, y int32) (red, green, blue int32) {
+	red = c[x][y].Colour.red
+	green = c[x][y].Colour.green
+	blue = c[x][y].Colour.blue
+	return
+}
+
+func (c *PixelArray) FilledAt(x, y int32) bool {
+	return c[x][y].Filled
+}
+
+func (c *PixelArray) QueuedAt(x, y int32) bool {
+	return c[x][y].Queued
+}
+
+func (c *PixelArray) TargetColourAt(x, y int32) (red, green, blue int32) {
+
+	var r_sum, g_sum, b_sum int32 // temps to hold the colours of surrounding pixels
+	var filled_neighbours int32
+	var x_offset, y_offset int32
+
+	for x_offset = -1; x_offset < 2; x_offset++ {
+		if x+x_offset < MaxWidth && x+x_offset >= 0 {
+			for y_offset = -1; y_offset < 2; y_offset++ {
+
+				if x_offset == 0 && y_offset == 0 {
+					//skip calling pixel
+					continue
+				}
+
+				if y+y_offset < MaxHeight && y+y_offset >= 0 {
+					if c.FilledAt(x+x_offset, y+y_offset) {
+						r, g, b := c.ColourAt(x+x_offset, y+y_offset)
+						r_sum += r
+						g_sum += g
+						b_sum += b
+						filled_neighbours++
+					}
 				}
 			}
 		}
 	}
 
-	k := int32(0)
-	red = 0
-	green = 0
-	blue = 0
-	for i := 0; i < n; i++ {
-		pt := pt_array[i]
-		current := pic.At(pt.X, pt.Y)
-		r, g, b, a := current.RGBA()
-		// returns values 8 orders of magnitude too high
-		r /= 256
-		g /= 256
-		b /= 256
-		if a > 0 {
-			k++
-			red += int32(r)
-			green += int32(g)
-			blue += int32(b)
-		}
-	}
-
-	if k != 0 {
-		red = red / k
-		green = green / k
-		blue = blue / k
+	if filled_neighbours > 0 {
+		red = r_sum / filled_neighbours
+		green = g_sum / filled_neighbours
+		blue = b_sum / filled_neighbours
 	}
 	return
 }
 
-type RGBCube [256][256][256]bool
-
-type QueuedArray [4096][4096]bool
-
+// main function does all the stuff in the right order
 func main() {
-	start := time.Now()
-	fmt.Println("starting...")
-	height := 4096
-	width := 4096
-
-	pic := image.NewNRGBA(image.Rect(0, 0, width, height))
-
 	colours := new(RGBCube)
 
-	queuedList := new(QueuedArray)
+	picture := new(PixelArray)
 
 	// changing channel size affects behaviour of colour filling;
 	// or rather, it makes CPU scheduling choices have a greater impact
-	c := make(chan image.Point, 256)
+	ch := make(chan image.Point, ChanSize)
 	// seed point
-	c <- image.Pt(2048, 2048)
-	queuedList[2048][2048] = true // pretend it got popped naturally
-	count := popAndDraw(pic, colours, c, queuedList)
-	end := time.Now()
+	ch <- image.Pt(StartX, StartY)
+	picture[StartX][StartY].Queued = true // pretend it got popped naturally
+	_ = fillPixelArray(picture, colours, ch)
 
-	logTimestamp(start, end, count)
+	draw(picture.ImageNRGBA())
 }
